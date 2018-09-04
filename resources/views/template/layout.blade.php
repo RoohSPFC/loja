5e60e0e865c00dde3b0e260ded34631554ba16dd
DESKTOP-28S484V\wsbrasil <estagiowsbrasil@bitbucket.org>
2018-09-04 16:38:09 -0300

    resolve | Criar Menu da loja #1
    Término 16:34:00 04/09/2018
    
    Tempo total 03:30:00

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -11,6 +11,35 @@
     <title>Document</title>
 </head>
 <body class="container">
+
+        <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            
+                            <span class="icon-bar">12</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                          <a class="navbar-brand" href="page.blade.html">Página Principal</a>
+                        </div>
+                    
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                          <ul class="nav navbar-nav">
+                            <li class="active"><a href="/produto/">Produtos <span class="sr-only">(current)</span></a></li>
+                            <li><a href="/produto/marca/">Marcas</a></li>
+                          </ul>
+                          <form class="navbar-form navbar-left">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                          </form>
+                        </div><!-- /.navbar-collapse -->
+                      </div><!-- /.container-fluid -->
+        </nav>
+
     <div class="form-group">
         @yield('content')
     </div>
